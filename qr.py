9fe2d2af70d1ce0824efbd755476f547f2ec0ac4
Kevin Cromley <kevincromley2020@gmail.com>
2024-07-07 21:22:22 -0700

    Some small scripts

--- qr.py
+++ qr.py
@@ -2,10 +2,10 @@
 import qrcode
  
 # Data to be encoded
-data = 'www.sandiegohaulers.com'
+data = 'https://didi-food.onelink.me/ssCr/redeemCode?code=FOOD-N9HMN9&activityType=1&locale=en-US'
  
 # Encoding data using make() function
 img = qrcode.make(data)
  
 # Saving as an image file
-img.save('MyQRCode1.png')
+img.save('didi.png')
